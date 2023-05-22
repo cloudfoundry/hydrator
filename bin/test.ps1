9305c703f69ee9ba3f0cdb23972b194b98cb4e9b
Amin Jamali <ajamali@vmware.com>
2023-05-22 15:36:16 +0000

    fix typo

--- bin/test.ps1
+++ bin/test.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
 ginkgo.exe -r --race -keep-going --randomize-suites
-if ($LASEXITCODE -ne 0) {
+if ($LASTEXITCODE -ne 0) {
     throw "tests failed"
 }
