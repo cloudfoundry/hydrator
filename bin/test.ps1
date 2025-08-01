fde1e43279d55a57f9b139263924551a31bd2a29
Amin Jamali <71907054+winkingturtle-vmw@users.noreply.github.com>
2025-08-01 09:44:40 -0400

    Update test.ps1
    to run ginkgo from go.mod, since ginkgo.exe doesn't exist on the worker.

--- bin/test.ps1
+++ bin/test.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-ginkgo.exe -r --race -keep-going --randomize-suites
+go run github.com/onsi/ginkgo/v2/ginkgo -r --race -keep-going --randomize-suites
 if ($LASTEXITCODE -ne 0) {
     throw "tests failed"
 }
